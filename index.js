194d00aaa4e73f0aa2ed68ebf05938d370592c33
Oge <subjectiverealityy@gmail.com>
2026-09-14 18:25:19 +0100

    validate division, handle division errors and normalise operands

--- index.js
+++ index.js
@@ -11,13 +11,22 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  if (b == 0) return "Error: You tried to divide a number by 0"
+  if (b === 0) throw new RangeError("Error: You tried to divide a number by 0")
   return a / b
 }
 
 function operate(operator, a, b) {
-  if (operator === 'add') return add(a, b)
-  else if (operator === 'subtract') return subtract(a, b)
-  else if (operator === 'multiply') return multiply(a, b)
-  else if (operator === 'divide') return divide(a, b)
+  a = Number(a)
+  b = Number(b)
+
+  try {
+    if (operator === 'add') return add(a, b)
+    else if (operator === 'subtract') return subtract(a, b)
+    else if (operator === 'multiply') return multiply(a, b)
+    else if (operator === 'divide') return divide(a, b)
+
+  } catch (error) {
+    if (error) return null
+    // Display an error message if operate() returns null
+  }
 }
